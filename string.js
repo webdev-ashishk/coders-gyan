4e5491ea4bcdbf19a033833641e626c7b51c18eb
webdev-ashishk <webdev.ashishk@gmail.com>
2023-04-20 11:56:43 +0530

    working with string

---
+++ string.js
@@ -0,0 +1,8 @@
+//replace method will not mutate original string
+//replace mehtod return new string and replace only match's
+const str = "please visit Google";
+const replaceStr = str.replace("Google", "microsoft");
+// console.log(str);
+// console.log(replaceStr);
+const replaceAll = str.replaceAll("Work hard for google");
+console.log(replaceAll);
